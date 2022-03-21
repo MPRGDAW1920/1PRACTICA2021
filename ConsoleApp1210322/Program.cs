cc50165da42f79e7125eceae58c18c8cbfe395ca
DAW1920PRG2 <amisecretaria@hotmail.com>
2022-03-21 17:16:35 +0100

    SUBO DOS CODIGOS

---
+++ ConsoleApp1210322/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1210322
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
